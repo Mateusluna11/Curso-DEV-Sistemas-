7d9eb49ecd1f75550a6d0c2af1b7eeb2116e1bdb
Mateusluna11 <166398210+Mateusluna11@users.noreply.github.com>
2024-04-11 08:37:46 -0300

    Create Questão3.py

---
+++ Classes e Objetos 1/Questão3.py
@@ -0,0 +1,36 @@
+#3 - Crie uma classe que modele uma pessoa:
+
+class Pessoa:
+    def __init__(self, nome, idade, peso, altura):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura
+
+    def envelhecer(self, anos):
+        self.idade += anos
+        if self.idade < 21:
+            self.crescer(0.5 * anos)
+
+    def engordar(self, kg):
+        self.peso += kg
+
+    def emagrecer(self, kg):
+        self.peso -= kg
+
+    def crescer(self, cm):
+        self.altura += cm
+
+    def imprimir_informacoes(self):
+        print(f"Nome: {self.nome}")
+        print(f"Idade: {self.idade} anos")
+        print(f"Peso: {self.peso} kg")
+        print(f"Altura: {self.altura} cm")
+
+pessoa1 = Pessoa("João", 18, 70, 170)
+pessoa1.imprimir_informacoes()
+
+pessoa1.envelhecer(3)
+pessoa1.engordar(5)
+pessoa1.crescer(2)
+pessoa1.imprimir_informacoes()
