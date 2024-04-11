640bf760130cbb8aec14b269f4059e5b0aee1d1b
Mateusluna11 <166398210+Mateusluna11@users.noreply.github.com>
2024-04-11 08:44:56 -0300

    Create Questão1.py

---
+++ Classes e Objetos 2/Questão1.py
@@ -0,0 +1,23 @@
+1-) Crie uma classe que modele uma pessoa:
+
+class Pessoa:
+    def __init__(self, nome, idade, endereco):
+        self.nome = nome
+        self.idade = idade
+        self.endereco = endereco
+
+    def mostrar_endereco(self):
+        print(f"Endereço: {self.endereco}")
+
+    def alterar_endereco(self, novo_endereco):
+        self.endereco = novo_endereco
+        print("Endereço alterado com sucesso.")
+
+pessoa = Pessoa(nome="Maria", idade=25, endereco="Rua A, 123")
+pessoa.mostrar_endereco()
+
+pessoa.alterar_endereco("Avenida B, 456")
+pessoa.mostrar_endereco()
+Endereço: Rua A, 123
+Endereço alterado com sucesso.
+Endereço: Avenida B, 456
