832fbcecb9bd31a40e4d7d0cb690b2bdf29cafa1
Mateusluna11 <166398210+Mateusluna11@users.noreply.github.com>
2024-04-11 09:13:55 -0300

    Create Questão7.py

---
+++ Estrutura sequencial /Questão7.py
@@ -0,0 +1,6 @@
+#7-)Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+print ('Área do quadrado!')
+lado = float (input('Diga o valor de um lado qualquer! >> '))
+area = lado ** 2
+print (f"{area}")
