f81eff96f7768b64e12fd066ac5aeebf0c5ecd71
Mateusluna11 <166398210+Mateusluna11@users.noreply.github.com>
2024-04-11 09:18:55 -0300

    Create Questão10.py

---
+++ Estrutura sequencial /Questão10.py
@@ -0,0 +1,8 @@
+#10-)Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
+print ("CONVERSÃO DE TEMPERATURA - by celsius to fahrenheit")
+
+celsius = float(input("Digite a temperatura em graus Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"A temperatura em graus Fahrenheit é {fahrenheit}")
