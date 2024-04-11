605625ab8ae8456fa375ee605c2d20617e536524
Mateusluna11 <166398210+Mateusluna11@users.noreply.github.com>
2024-04-11 09:09:12 -0300

    Create Questão4.py

---
+++ Estrutura sequencial /Questão4.py
@@ -0,0 +1,10 @@
+#4-)Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+notaUm = float (input("Nota 1° bimestre: "))
+notaDois = float (input("Nota 2° bimestre: "))
+notaTres = float (input("Nota 3° bimestre: "))
+notaQuatro = float (input("Nota 4° bimestre: "))
+
+media = (notaUm + notaDois + notaTres + notaQuatro) / 4
+
+print (f"A média das notas é {media}")
