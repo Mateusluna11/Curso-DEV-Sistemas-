a2d56ec677922ee83235d5ad21cb67c86a4d6f91
Mateusluna11 <166398210+Mateusluna11@users.noreply.github.com>
2024-04-11 09:07:48 -0300

    Create Questão3.py

---
+++ Estrutura sequencial /Questão3.py
@@ -0,0 +1,6 @@
+#3-) Faça um Programa que peça dois números e imprima a soma.
+
+numUm = int (input ("Digite o número 1: "))
+numDois = int (input ("Digite o número 2: "))
+soma = numUm + numDois
+print (f"Soma realizada: {soma}")
