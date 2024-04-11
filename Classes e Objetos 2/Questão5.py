41e4a8eaa82be304f995c9ed652727a270ef5511
Mateusluna11 <166398210+Mateusluna11@users.noreply.github.com>
2024-04-11 08:51:14 -0300

    Create Questão5.py

---
+++ Classes e Objetos 2/Questão5.py
@@ -0,0 +1,18 @@
+5-) Crie uma classe que modele um carro: (a) Atributos: marca, ano e preco. (b) Metodos: mostrar preco e de exibicao dos dados.
+
+class Carro:
+    def __init__(self, marca, ano, preco):
+        self.marca = marca
+        self.ano = ano
+        self.preco = preco
+
+    def mostrar_preco(self):
+        return f"O preço do carro {self.marca} ({self.ano}) é R$ {self.preco}"
+
+    def exibir_dados(self):
+        return f"Marca: {self.marca}\nAno: {self.ano}\nPreço: R$ {self.preco}"
+
+carro_exemplo = Carro(marca="Fiat", ano=2022, preco=75000)
+print(carro_exemplo.mostrar_preco())
+print("\nDetalhes do Carro:")
+print(carro_exemplo.exibir_dados())
