61e2bf4539627059e44f0efe74d8dca42e7f6014
Mateusluna11 <166398210+Mateusluna11@users.noreply.github.com>
2024-04-13 23:42:58 -0300

    Delete Dicionários em Pytho directory

--- Dicionários em Pytho/Q01.Programa.1Dicionário.py
+++
@@ -1,8 +0,0 @@
-#1- Faça um programa que possua um Dicionário, adicione elementos ao dicionário e os mostre na tela.
-dicio = {}
-
-dicio['nome'] = 'João'
-dicio['idade'] = 25
-dicio['cidade'] = 'Recife'
-
-print("Dicionário:", dicio)
