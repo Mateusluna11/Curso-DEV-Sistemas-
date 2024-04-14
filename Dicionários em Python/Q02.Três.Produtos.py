c5cb87ecd8341446b7e4b45681ebf8e8336686e8
Mateusluna11 <166398210+Mateusluna11@users.noreply.github.com>
2024-04-13 23:29:44 -0300

    Create Q02.Três.Produtos.py

---
+++ Dicionários em Python/Q02.Três.Produtos.py
@@ -0,0 +1,14 @@
+#2 -  Faça um programa, utilizando Dicionários,
+#que peça para o usuário inserir o nome de três produtos de mercado e seus respectivos preços e os mostre na tela.
+
+produtos = {}
+
+for i in range(3):
+    produto = input(f"Insira o nome do {i + 1} produto: ")
+    preco = float(input(f"Insira o preço de {produto}: R$ "))
+    produtos[produto] = preco
+
+print("\nProdutos e Preços:")
+for produto, preco in produtos.items():
+    print(f"{produto}: R$ {preco:.2f}")
+
