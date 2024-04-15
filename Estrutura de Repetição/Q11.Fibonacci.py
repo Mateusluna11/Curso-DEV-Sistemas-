6913e2729ee9421e887eae0ea51f19e811656a7a
Mateus Luna <166398210+Mateusluna11@users.noreply.github.com>
2024-04-15 09:07:05 -0300

    Create Q11.Fibonacci.py

---
+++ Estrutura de Repetição/Q11.Fibonacci.py
@@ -0,0 +1,8 @@
+#11 - A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série até o n−ésimo termo.
+
+n = int(input("Digite o valor de n para a série de Fibonacci: "))
+
+a, b = 1, 1
+for _ in range(n):
+    print(a, end=" ")
+    a, b = b, a + bbb
