ce0962dc35e35a0133a6d82bcd0f4f47b547078d
Mateus Luna <166398210+Mateusluna11@users.noreply.github.com>
2024-04-15 19:54:41 -0300

    Create Q05.Três.Numeros.Maior.py

---
+++ Estrutura de Comparação/Q05.Três.Numeros.Maior.py
@@ -0,0 +1,8 @@
+#5 - Faça um Programa que leia três números e mostre o maior deles.
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+maior = max(num1, num2, num3)
+print("O maior número é:", maior)
