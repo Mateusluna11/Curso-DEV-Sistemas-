831c53fcfb8294694d5fe05a6a2b9ad39b1132bd
Mateus Luna <166398210+Mateusluna11@users.noreply.github.com>
2024-04-15 08:42:48 -0300

    Create Q03.Programa.1.a.20.py

---
+++ Estrutura de Repetição/Q03.Programa.1.a.20.py
@@ -0,0 +1,7 @@
+#3 - Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro.
+for num in range(1, 21):
+    print(num)
+
+# Um ao lado do outro
+for num in range(1, 21):
+    print(num, end=" ")
